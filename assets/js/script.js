ac227af424877ae552f873c95b2ceda510aec348
MD. MONWAR SIDDIQ <monwarsiddiq@gmail.com>
2022-02-06 00:31:16 +0600

    Changing design V1.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,13 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("header").style.padding = "0px"
-    document.getElementById("header").style.backgroundColor = "#000000"
-  } else {
-    document.getElementById("header").style.padding = "10px 0px"
-    document.getElementById("header").style.backgroundColor = "transparent"
-  }
+   if(window.innerWidth >= 1200) {
+       if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+          document.getElementById("header").style.padding = "0px"
+          document.getElementById("header").style.backgroundColor = "#000000"
+       }else {
+          document.getElementById("header").style.padding = "10px 0px"
+          document.getElementById("header").style.backgroundColor = "transparent"
+       }
+   }
 }
-
-$('.slider-image').owlCarousel({
-    loop:true,
-    nav:true,
-    dots: true,
-    autoplay: false
-})
-
-$('.grid').masonry({
-  // options
-  itemSelector: '.grid-item-1',
-  columnWidth: col-sm-6
-});
-
-$(".portfolio-list").isotope({
-  
-});
